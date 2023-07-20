9e4b3602f6d62db1f0da43610e34eea7c60ded77
paoloardissone <pla.ardissone@gmail.com>
2023-07-20 20:13:54 +0200

    Initial commit

---
+++ src/test/java/com/calced/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.calced.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
